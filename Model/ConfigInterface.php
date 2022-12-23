94d1af89121951fb18491093fad17f1e4bc929b0
Pablo Fornali <pablo@magestio.com>
2022-12-23 12:06:19 +0100

    Init Magestio Whatsapp repo

---
+++ Model/ConfigInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Magestio\Whatsapp\Model;
+
+/**
+ * Interface ConfigInterface
+ */
+interface ConfigInterface
+{
+    const XML_PATH_WHATSAPP_ACTIVE = "mwhatsapp/whatsapp/active";
+    const XML_PATH_WHATSAPP_PHONE = "mwhatsapp/whatsapp/phone";
+}
